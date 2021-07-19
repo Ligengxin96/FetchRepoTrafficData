1990889b1b41d9b87f3feee8f3591667275f8cd8
Ligengxin <Ligengxin96@gmail.com>
2021-07-19 23:26:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use('/getrepoinfo/v1/getrepoinfo', trafficDataRouter);
 
 app.listen(port, () => {
-  console.log(`${new Date()} Server running on port: ${port}`);
+  console.log(`${new Date()}: Server running on port: ${port}`);
 });
 
 
