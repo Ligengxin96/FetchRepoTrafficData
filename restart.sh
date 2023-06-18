43185d084f139e4e733e9ed417a05ffb642f4340
Ligengxin <Ligengxin96@gmail.com>
2023-06-18 00:09:40 -0400

    update

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker stop fetchRepoTrafficData && docker rm fetchRepoTrafficData
-docker run -v /etc/localtime:/etc/localtime --name fetchRepoTrafficData --network userDefined --ip 172.20.0.6 -d fetch-repo-traffic-image
+docker run -v /etc/localtime:/etc/localtime --name fetchRepoTrafficData --network userDefined -d fetch-repo-traffic-image
